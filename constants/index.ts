05422aeaab73814dee6f5c959500070204a1fbdb
SardarAwais88 <khanowais8888@gmail.com>
2023-11-27 18:23:16 +0500

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,279 @@
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/Space Portfolio Assets/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/Space Portfolio Assets/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/Space Portfolio Assets/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/Space Portfolio Assets/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/Space Portfolio Assets/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/Space Portfolio Assets/redux.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/Space Portfolio Assets/reactquery.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/Space Portfolio Assets/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/Space Portfolio Assets/next.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Framer Motion",
+      Image: "/Space Portfolio Assets/framer.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Stripe Payment",
+      Image: "/Space Portfolio Assets/stripe.webp",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Node js",
+      Image: "/Space Portfolio Assets/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/Space Portfolio Assets/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/Space Portfolio Assets/instagram.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/Space Portfolio Assets/facebook.svg",
+     
+    },
+    {
+      name: "Instagram",
+      src: "/Space Portfolio Assets/discord.svg",
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+    {
+      skill_name: "Html 5",
+      Image: "/Space Portfolio Assets/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/Space Portfolio Assets/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/Space Portfolio Assets/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/Space Portfolio Assets/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Material UI",
+      Image: "/Space Portfolio Assets/mui.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/Space Portfolio Assets/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/Space Portfolio Assets/redux.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/Space Portfolio Assets/reactquery.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/Space Portfolio Assets/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/Space Portfolio Assets/next.png",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Node js",
+      Image: "/Space Portfolio Assets/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Express js",
+      Image: "/Space Portfolio Assets/express.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/Space Portfolio Assets/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+    {
+      skill_name: "Fire base",
+      Image: "/Space Portfolio Assets/Firebase.png",
+      width: 55,
+      height: 55,
+    },
+    {
+      skill_name: "Postger SQL",
+      Image: "/Space Portfolio Assets/postger.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "My SQL",
+      Image: "/Space Portfolio Assets/mysql.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Prisma",
+      Image: "/Space Portfolio Assets/prisma.webp",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Graphql",
+      Image: "/Space Portfolio Assets/graphql.png",
+      width: 80,
+      height: 80,
+    },
+    {
+        skill_name: "Python Django",
+        Image: "/Space Portfolio Assets/python django.png",
+        width: 80,
+        height: 80,
+      },
+
+      {
+        skill_name: "Python Flask",
+        Image: "/Space Portfolio Assets/Python Flask.png",
+        width: 80,
+        height: 80,
+      },
+
+  
+  ];
+  
+  export const Full_stack = [
+    {
+      skill_name: "React Native",
+      Image: "/Space Portfolio Assets/ReactNative .png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Tauri",
+      Image: "/Space Portfolio Assets/tauri.svg",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Docker",
+      Image: "/Space Portfolio Assets/docker.webp",
+      width: 70,
+      height: 70,
+    },
+  
+    {
+      skill_name: "Figma",
+      Image: "/Space Portfolio Assets/figma.png",
+      width: 50,
+      height: 50,
+    },
+  
+  ];
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "Go",
+      Image: "/Space Portfolio Assets/go.png",
+      width: 60,
+      height: 60,
+    },
+    {
+        skill_name: "Python",
+        Image: "/Space Portfolio Assets/Python.png",
+        width: 100,
+        height: 100,
+      },
+  ];
+  
+  
+  
+  
+  
